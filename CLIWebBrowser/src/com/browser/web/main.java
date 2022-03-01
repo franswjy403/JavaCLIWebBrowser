2c212ca8ce9c9767d581316d0f604f9a4d6e571f
James Lee <jamesrafe10@gmail.com>
2022-03-02 00:59:33 +0700

    Basic Auth, Redirection, Error code, and 1/2 of links

--- CLIWebBrowser/src/com/browser/web/main.java
+++ CLIWebBrowser/src/com/browser/web/main.java
@@ -4,6 +4,8 @@ import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Base64;
 import java.util.Scanner;
 
 public class main {
@@ -17,13 +19,14 @@ public class main {
 			if (choice == 1) {
 				input.nextLine();
 				String url = input.nextLine();
-				getWebsite(url);
+				String respo = getWebsite(url, false, null);
+				System.out.println(respo);
 			}
 		} while (choice != 0);
 
 	}
 
-	public static void getWebsite(String url) {
+	public static String getWebsite(String url, boolean auth, String autho) {
 		try {
 			String domain = url.split("/")[0];
 			String path = "";
@@ -37,6 +40,9 @@ public class main {
 			BufferedOutputStream bos = new BufferedOutputStream(socket.getOutputStream());
 			
 			String req = "GET "+path+" HTTP/1.1\r\nHost: "+domain+"\r\n\r\n";
+			if(auth) {
+				req = "GET "+path+" HTTP/1.1\r\nAuthorization: Basic "+autho+"\r\nHost: "+domain+"\r\n\r\n";
+			}
 			System.out.println(req);
 			bos.write(req.getBytes());
 			// bos.write("GET /index.php/berita/lihatBerita\r\n\r\n".getBytes());
@@ -53,34 +59,83 @@ public class main {
 				bResp = new byte[bufferSize];
 				c = bis.read(bResp);
 			}
+			
+			socket.close();
 
 			String[] basket = resp.split("\n");
 			String[] indiv = basket[0].split(" ");
+			String code = indiv[1];
+			String respo = "";
 			   
-			if(checkError(indiv)) {
-				System.out.println(resp);
+			if(code.charAt(0)=='2') {
+				respo = resp;
+			}
+			else if(code.equals("401")) {
+				Scanner input = new Scanner(System.in);
+				String user, pass;
+				System.out.println("Username: ");
+				user = input.nextLine();
+				System.out.println("Password: ");
+				pass = input.nextLine();
+				
+				String authString = user + ":" + pass;
+				
+				String authEncBytes = Base64.getEncoder().encodeToString(authString.getBytes());
+				respo = getWebsite(url, true, authEncBytes);
 			}
 			else {
-				for (int i = 1; i<indiv.length; i++)
-				System.out.print(indiv[i]+' ');
+				for (int i = 1; i<indiv.length; i++) {
+					respo += (indiv[i]+' ');
+				}
 			}
-
-			socket.close();
+			
+			String[] redi = basket[4].split(";");
+			if(redirect(redi)) {
+				redi = redi[1].split(":");
+				System.out.println(redi[1]);
+				String fin = redi[1].substring(2, redi[1].length()-1);
+				respo = getWebsite(fin, false, null);
+			}
+			
+			
+			ArrayList<String> links = checkLink(respo);
+			for(int i = 0; i<links.size(); i++) {
+				respo += ("\n" + links.get(i));
+			}
+			
+			return respo;
 
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			System.out.println("There is an error occured");
 			e.printStackTrace();
 		}
+		return "Not working somehow";
 	}
 	
-	 public static boolean checkError(String[] ind) {
-		 if(ind[1].charAt(0) == '2') {
+	 public static char checkError(String[] ind) {
+			 return ind[1].charAt(0);
+	}
+
+	 
+	 public static boolean redirect(String[] ind) {
+		 if(ind.length>1 && ind[1].contains("url")) {
 			 return true;
 		 }
 		 else {
 			 return false;
 		 }
+	 }
+	 
+	 public static ArrayList<String> checkLink(String ind) {
+		String[] basket = ind.split("\n");
+		ArrayList<String> linkz = new ArrayList<String>();
+		for (int i = 0; i<basket.length; i++) {
+				if(basket[i].contains("<a ")) {
+					linkz.add(basket[i]);
+				}
+		}
+		return linkz; 
 	 }
 
 }
